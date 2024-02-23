97fd172ae977d1661584d995870a69c490074f7a
Juniper <juniperwrenarts@gmail.com>
2024-02-22 23:10:45 -0500

    added comments

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
-const express = require('express');
-const db = require('./config/connection');
-const routes = require('./routes');
+// Importing required modules
+const express = require('express'); // Importing Express.js framework
+const db = require('./config/connection'); // Importing database connection
+const routes = require('./routes'); // Importing route handlers
 
+// Setting up port number
 const PORT = process.env.PORT || 3001;
+
+// Creating an Express application
 const app = express();
 
+// Middleware to parse incoming requests with JSON payloads
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Middleware to use defined routes
 app.use(routes);
 
+// Once the database connection is open, start the server
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
